9d63d61a447ee7489b91fabe7b4f2c4af77afa47
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-03-06 17:51:43 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) {
+	int a[1000],i,j,t,lim;
+	scanf("%d",&lim);
+	for(i=0;i<lim;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<limit;i++)
+	{
+		for(j=0;j<limt;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[j];
+				a[j]=a[i];
+				a[i]=t;
+			 }
+			
+		}
+	}
+	printf("%d\t",a[limit%2]);
+	return 0;
+}
