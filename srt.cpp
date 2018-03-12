dc2ac50bf2ce45191091c85b08307c34d3010ba7
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-03-12 17:23:45 +0530

    Create srt.cpp

---
+++ srt.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+ char str1[2],str[2],str[2];
+ scanf("%d %d %d",str1,str2,str3);
+ if((str1[0]=str2[0]=str3[0])||(str1[1]=str2[1]=str3[1]))
+ {
+  printf("Yes");
+ }
+ else if((str1[1]=str2[1]=str3[1])||str1[2]=str2[2]=str3[2]))
+ {
+  printf("Yes");
+ }
+ else
+ {
+  printf("No");
+ }
+ return 0;
+ }
+  
