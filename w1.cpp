7415d0ab0e3a27a6e804a621166ff9b9f5d2a905
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-03-12 17:35:27 +0530

    Create w1.cpp

---
+++ w1.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+int main()
+{
+ string s1,s2;
+ cut<<"strings";
+ cin>>s1>>s2;
+ int s=s1.length(),r=s2.length();
+ if(s==r)
+ {
+  cout<<"String are equal";
+  { 
+  cout<<"String";
+  }
+ }
+ else
+ {
+  cout<<"Strings are not equal";
+  {
+   cout<<"String  is not equal";
+  }
+ }
+} 
