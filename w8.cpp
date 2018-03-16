36c5c6e33bab5b560e06d0bbe8bf57185e06d753
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-03-16 17:21:20 +0530

    Create w8.cpp

---
+++ w8.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+ char a[100];
+ printf("enter the character ");
+ scanf("%d",&a);
+	for(i=0;i<=a;i++)
+	{
+		if(num="")
+		{
+			count++;
+		}
+		else
+		{
+			printf("print the number of letters ");
+ 		}
+		}
+	}
+ 
