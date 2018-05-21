c6e9c500df9d14be29425a40ebf1ee7645da1433
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-05-21 10:29:41 +0530

    Update sp.cpp

--- sp.cpp
+++ sp.cpp
@@ -1,15 +1,15 @@
-#include<iostream.h>
+#include<iostream>
 void main()
 {
  string s[50],i,c=0;
- cout<<"enter the input";
- cin>>s;
+ Cout<<"enter the input";
+ Cin>>s;
  for(i=0;s[i]=0;i++)
  {
   if(s[i]=' ')
   {
    c++;
   }
-  cout<<c;
+  Cout<<c;
  }
  }
