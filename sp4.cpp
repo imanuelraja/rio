9bc304d17c58293aede50faf034516e6b238709a
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-05-22 10:16:24 +0530

    yy

---
+++ sp4.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("a:");
+scanf("%d",&a);
+printf("b:");
+scanf("%d",&b);
+c=a;
+a=b;
+b=c;
+printf("a=:%d b=:%d",a,b);
+getch();
+}
