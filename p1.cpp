da77eb4e743e04da9c73555d0177c1975453c194
Harini Ramasamy <36298776+imanuelraja@users.noreply.github.com>
2018-05-24 14:07:02 +0530

    p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+int main()
+{
+ char str1,str2;
+ int k,count=0;
+ cin>>str1;
+ cin>>str2;
+ cin>>k;
+ for(i=0;i<=str.length();i++)
+ {
+ if(str1[i]!=str2[i])
+ {
+   count++;
+  }
+ }
+ if(count==k)
+ { 
+ cout<<"Yes";
+ }
+ else
+ {
+  cout<<"No";
+ }
+}
